2c5e236eccd8beb86ee68c1a53334de46b6c73f5
Berg72 <bergy450r@gmail.com>
2024-02-27 13:47:53 -0800

    Update GalleryView to display a larger version of selected photo

--- Restaurant/GalleryView.swift
+++ Restaurant/GalleryView.swift
@@ -10,37 +10,45 @@ import SwiftUI
 struct GalleryView: View {
     
     @State var photoData = [String]()
+    @State var sheetVisible = false
     var dataService = DataService()
     
     var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text("Gallery")
+                .font(.largeTitle)
+                .bold()
             
-            VStack(alignment: .leading) {
-                Text("Gallery")
-                    .font(.largeTitle)
-                    .bold()
-                
-                GeometryReader { proxy in
+            GeometryReader { proxy in
                 
                 ScrollView {
                     LazyVGrid(columns: [GridItem(spacing: 10.0),
                                         GridItem(spacing: 10.0),
                                         GridItem(spacing: 10.0)],
-                                        spacing: 10.0) {
+                              spacing: 10.0) {
                         ForEach(photoData, id: \.self) { p in
                             Image(p)
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .frame(maxWidth: (proxy.size.width-20)/3)
                                 .clipped()
+                                .onTapGesture {
+                                    sheetVisible = true
+                                }
                         }
                     }
-                    }
                 }
-            }.padding(.horizontal)
+            }
+        }.padding(.horizontal)
             .onAppear {
                 photoData = dataService.getPhotos()
-            
-        }
+                
+            }
+            .sheet(isPresented: $sheetVisible) {
+                PhotoView()
+                
+            }
     }
 }
 
---
+++ Restaurant/PhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoView.swift
+//  Restaurant
+//
+//  Created by Mark Bergeson on 2/27/24.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoView()
+}
